4cb8967ea95957a186952cee5dc0e4646f24852b
Marcelo Cerritos <mcerritos@botblaze.tech>
2025-03-27 10:02:18 -0600

    Feat: Cambiar icono y texto de estado a "Intermitente" en el dashboard

--- resources/views/business/dashboard/index.blade.php
+++ resources/views/business/dashboard/index.blade.php
@@ -45,11 +45,11 @@
                     <div
                         class="flex w-full flex-1 items-center justify-center gap-4 rounded-lg border border-gray-300 p-4 dark:border-gray-800 dark:bg-gray-950">
                         <span class="rounded-full bg-green-100 p-4 dark:bg-green-950/30">
-                            <x-icon icon="circle-check" class="size-10 text-green-500 sm:size-12" />
+                            <x-icon icon="warning" class="size-10 text-green-500 sm:size-12" />
                         </span>
                         <div class="flex flex-col items-center justify-center gap-1 sm:items-start">
-                            <p class="text-2xl font-bold text-green-500">
-                                Operativo
+                            <p class="text-2xl font-bold text-yellow-500">
+                                Intermitente
                             </p>
                             <h1 class="text-sm text-gray-500 dark:text-gray-300">
                                 Estado hacienda
--- resources/views/components/icon.blade.php
+++ resources/views/components/icon.blade.php
@@ -178,6 +178,8 @@
             '<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-minus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><line x1="5" y1="12" x2="19" y2="12" /></svg>',
         'download' =>
             '<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-download"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5v13m-4 -4l4 4l4 -4m-4 4v-10" /></svg>',
+        'warning' =>
+            '<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-filled icon-tabler-warning"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 9v2m0 4v.01" /><path d="M12 3v.01" /><path d="M12 18v.01" /><path d="M3 20h18a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-18a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2" /><path d="M9 7l3 -3l3 3l3 3" /></svg>',
     ];
 
     $iconSvg = $icons[$icon] ?? null;
