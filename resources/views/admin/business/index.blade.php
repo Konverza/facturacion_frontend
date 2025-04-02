c52e579a956d4994c0dea4e95d7ab761658464b1
Marcelo Cerritos <jose.marcelo.1706@gmail.com>
2025-04-02 16:15:54 -0600

    Fix: Corregir enlace de edición en la vista de negocios para usar el ID correcto

--- resources/views/admin/business/index.blade.php
+++ resources/views/admin/business/index.blade.php
@@ -107,7 +107,7 @@
                                                 </button>
                                             </li>
                                             <li>
-                                                <a href="{{ Route('admin.business.edit', 1) }}"
+                                                <a href="{{ Route('admin.business.edit', $busines->id) }}"
                                                     class="flex w-full items-center gap-1 rounded-lg px-2 py-2 text-gray-600 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-900">
                                                     <x-icon icon="pencil" class="h-4 w-4" />
                                                     Editar
